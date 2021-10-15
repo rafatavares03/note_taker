125f4f296da4dce421ecabbdeb943c53a32a65da
Rafael Tavares <rafatavares03@hotmail.com>
2021-10-14 21:33:18 -0300

    some changes in the script

--- assets/js/script.js
+++ assets/js/script.js
@@ -2,6 +2,9 @@ const NotesArea = document.querySelector('#notesArea');
 const modal = document.querySelector('#modal-popup');
 const modalContent = document.querySelector('#modal-content');
 
+
+
+
 function NoteAreaIsEmpty() {
     if (NotesArea.childNodes.length === 0) {
         const messageText = document.createTextNode(`Nenhuma nota foi criada ainda!`);
@@ -14,27 +17,36 @@ function NoteAreaIsEmpty() {
     }
 }
 
+
 NoteAreaIsEmpty();
 
-function createViewButton(text) {
+
+function createViewButton() {
     const button = document.createElement('input');
     button.type = "button";
     button.value = "Ver Detalhes";
     button.classList.add("button");
-    button.onclick = () => {
+    button.onclick = (e) => {
+        const parent = e.target.parentNode;
+        const title = parent.firstChild.innerText;
+        const text = e.target.previousSibling.innerText;
+
+        document.querySelector('#noteTitle').innerHTML = title;
         modalContent.innerHTML = text;
         modal.classList.add("isVisible");
     }
     return button;
 }
 
+
 function createNote(note) {
     const noteArea = document.createElement('div');
     const noteTitle = document.createElement('h1');
     const noteParagraph = document.createElement('p');
     const noteText = document.createTextNode(note);
-    const titleText = document.createTextNode('Nota ' + (NotesArea.childNodes.length + 1));
-    const viewButton = createViewButton(note);
+    const title = 'Nota ' + (NotesArea.childNodes.length + 1);
+    const titleText = document.createTextNode(title);
+    const viewButton = createViewButton();
 
 
     noteArea.classList.add('note');
@@ -51,6 +63,15 @@ function createNote(note) {
 }
 
 
+function CleanPopUp() {
+    const noteTitle = document.querySelector('#noteTitle');
+
+    noteTitle.removeChild(noteTitle.firstChild);
+    modalContent.removeChild(modalContent.firstChild);
+    modal.classList.remove("isVisible");
+}
+
+
 
 
 document.querySelector('#submitNote').addEventListener('click', function submitNote() {
@@ -64,20 +85,17 @@ document.querySelector('#submitNote').addEventListener('click', function submitN
 });
 
 document.querySelector('#closePop-Up').addEventListener('click', () => {
-    modalContent.removeChild(modalContent.firstChild);
-    modal.classList.remove("isVisible");
+    CleanPopUp();
 });
 
 document.addEventListener('click', (e) => {
     if (e.target === document.querySelector('.modal.isVisible')) {
-        modalContent.removeChild(modalContent.firstChild);
-        modal.classList.remove('isVisible');
+        CleanPopUp();
     }
 });
 
 document.addEventListener('keyup', (e) => {
     if (e.key === "Escape" && document.querySelector('.modal.isVisible')) {
-        modalContent.removeChild(modalContent.firstChild);
-        modal.classList.remove('isVisible');
+        CleanPopUp();
     }
 });
