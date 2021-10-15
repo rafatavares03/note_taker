431dc9becdb41b6ebab6b0eac46edc18bf52bab7
Rafael Tavares <rafatavares03@hotmail.com>
2021-10-15 11:45:24 -0300

    user can write the note title

--- assets/js/script.js
+++ assets/js/script.js
@@ -20,12 +20,18 @@ function NoteAreaIsEmpty() {
 
 NoteAreaIsEmpty();
 
-
-function createViewButton() {
+function buttonBase (value, cssClass) {
     const button = document.createElement('input');
     button.type = "button";
-    button.value = "Ver Detalhes";
-    button.classList.add("button");
+    button.value = value;
+    button.classList.add(cssClass);
+
+    return button;
+}
+
+
+function createViewButton() {
+    const button = buttonBase("Ver Detalhes", "button");
     button.onclick = (e) => {
         const parent = e.target.parentNode;
         const title = parent.firstChild.innerText;
@@ -38,23 +44,32 @@ function createViewButton() {
     return button;
 }
 
+function createRemoveButton() {
+    const button = buttonBase('\u2716', "remove");
+    button.onclick = e => e.target.parentNode.remove();
+
+    return button;
+}
+
 
 function createNote(note) {
     const noteArea = document.createElement('div');
     const noteTitle = document.createElement('h1');
     const noteParagraph = document.createElement('p');
     const noteText = document.createTextNode(note);
-    const title = 'Nota ' + (NotesArea.childNodes.length + 1);
+    const title = document.querySelector('#noteTitle').value;
     const titleText = document.createTextNode(title);
     const viewButton = createViewButton();
+    const removeButton = createRemoveButton();
 
 
     noteArea.classList.add('note');
-    noteTitle.appendChild(titleText);
+    noteTitle.appendChild(title);
     noteParagraph.appendChild(noteText);
     noteArea.appendChild(noteTitle);
     noteArea.appendChild(noteParagraph);
     noteArea.appendChild(viewButton);
+    noteArea.appendChild(removeButton);
     NotesArea.appendChild(noteArea);
     
     
@@ -76,11 +91,12 @@ function CleanPopUp() {
 
 document.querySelector('#submitNote').addEventListener('click', function submitNote() {
     const note = document.querySelector('#note').value;
-    if(note !== '') {
+    const title = document.querySelector('#noteTitle').value;
+    if(note !== '' && title !== '') {
         NoteAreaIsEmpty();
         createNote(note);
     } else {
-        window.alert('Erro! Não é possível criar notas vazias.');
+        window.alert('Erro! Não é possível criar notas sem título ou sem texto.');
     }
 });
 
